f81215774e367f3dec0882464d76e73f94addedd
Sidduu-Hiremath <104383952+Sidduu-Hiremath@users.noreply.github.com>
2024-09-26 12:11:03 +0530

    Update and rename ColorCoder.cpp to ColorCode.cpp

---
+++ ColorCode.cpp
@@ -0,0 +1,28 @@
+#include "ColorCode.h"
+#include <iostream>
+
+namespace TelCoColorCoder 
+{
+    const int numberOfMajorColors = 5;
+    const int numberOfMinorColors = 5;
+
+    ColorPair GetColorFromPairNumber(int pairNumber) {
+        int zeroBasedPairNumber = pairNumber - 1;
+        MajorColor majorColor = 
+            (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+        MinorColor minorColor =
+            (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+        return ColorPair(majorColor, minorColor);
+    }
+
+    int GetPairNumberFromColor(MajorColor major, MinorColor minor) {
+        return major * numberOfMinorColors + minor + 1;
+    }
+
+    void PrintColorCodeManual() {
+        for (int i = 1; i <= numberOfMajorColors * numberOfMinorColors; i++) {
+            ColorPair colorPair = GetColorFromPairNumber(i);
+            std::cout << i << ": " << colorPair.ToString() << std::endl;
+        }
+    }
+}
