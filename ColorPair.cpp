8afef312d9f4cbf058198a957f984a09943ad7a7
Sidduu-Hiremath <104383952+Sidduu-Hiremath@users.noreply.github.com>
2024-09-26 12:15:47 +0530

    Update ColorPair.cpp

--- ColorPair.cpp
+++ ColorPair.cpp
@@ -1,6 +1,6 @@
 #include "ColorPair.h"
 
-namespace TelCoColorCoder 
+namespace TelCoColorCoder  
 {
     const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
